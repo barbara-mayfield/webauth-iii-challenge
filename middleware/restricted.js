a027e3e9b91ee4638be9744d6b96da3e857ce875
Barbara Mayfield <bmayfield1991@gmail.com>
2020-01-21 18:54:49 -0600

    Working on protected route to get users

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,9 +1,17 @@
+const jwt = require("jsonwebtoken")
+
 module.exports = () => {
-    return (req, res, next) => {
-        if(!req.session || !req.session.user) {
-          return res.status(401).json({ message: "Invalid credentials" })
-        }
-        
-        next()
+  return (req, res, next) => {
+    try {
+      const token = req.headers.authorization
+      const decoded = jwt.verify(token, process.env.JWT_TOKEN)
+
+      req.userId = decoded.subject
+      next()
+    } catch (err) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      })
     }
   }
+}
